8124ec4d4f7cbc9f2ba95438dfa783e43b8d4883
Cesar Kastli <cesarkastli@gmail.com>
2018-05-11 17:05:37 -0300

    se agrega react-router

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Link, Switch, Route } from 'react-router-dom'
+import HomePage from '../pages/HomePage'
+import MoviesPage from '../pages/MoviesPage'
+import NotFoundPage from '../pages/NotFoundPage'
 
 const App = () =>
   <div>
-    <h1>¡Funciona!</h1>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/movies">Movies</Link></li>
+      <li><Link to="/inexistente">Inexistente</Link></li>
+    </ul>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/movies" component={MoviesPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   </div>
 
 export default App
---
+++ src/pages/MoviesPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const MoviesPage = () =>
+  <div>
+    <h1>Movies</h1>
+  </div>
+
+export default MoviesPage
